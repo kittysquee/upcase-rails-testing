4f2188cb1f0b140777027bec84d1914663c59d20
Elaine Osbourn <kittysquee@gmail.com>
2017-12-01 11:16:11 +0000

    Adds email to todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130162516) do
+ActiveRecord::Schema.define(version: 20171201111448) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
